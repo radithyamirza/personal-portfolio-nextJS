dd0ab39c7d82632d085485dd424b69abd75b54cb
Radithya Mirza Aribowo <59699584+radithyamirza@users.noreply.github.com>
2025-10-01 09:53:11 +0000

    Fixed metadata typo

--- radithya-personal-portfolio-app/app/layout.tsx
+++ radithya-personal-portfolio-app/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Radithya's Protfolio",
+  title: "Radithya's Portfolio",
   description: "Radithya's software engineering portfolio",
 };
 
